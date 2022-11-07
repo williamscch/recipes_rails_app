7b059519d8f07d27cfa51db2c754c6426325c148
wdavidcch <david.13.2121@gmail.com>
2022-11-07 17:24:38 -0400

    add validation rules

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,4 +1,8 @@
 class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
+
+  validates :name, presence: true, length: { maximum: 30, too_long: 'Maximun for lenght is 30 char' }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, precense: true
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, precense: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   has_many :recipes
   has_many :foods
+
+  validates :name, presence: true, length: { maximum: 30, too_long: 'Maximun for lenght is 30 char' }
 end
