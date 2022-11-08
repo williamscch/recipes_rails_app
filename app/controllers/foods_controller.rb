b171f23e58e5bf80d4b63ea3774079e22792c16c
nimplay <nimrod7day@gmail.com>
2022-11-08 19:26:19 -0400

    add and remove food

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,9 +3,26 @@ class FoodsController < ApplicationController
     @foods = Food.all
   end
 
+  def new
+    @user = current_user
+    @food = Food.new
+  end
+
   def create
-    @food = Food.new(food_params)
+    @food = Food.new(params.require(:food).permit(:name, :measurement_unit, :price, :quantity))
+    @food.user_id = current_user.id
+
     @food.save
+
+    if @food.save
+      redirect_to foods_path
+    end
+  end
+
+  def destroy
+    @food = Food.find(params[:id])
+    @food.destroy
     redirect_to foods_path
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,7 @@ Rails.application.routes.draw do
    root 'users#index'
    get 'users/:id', to: 'users#show', as: :user_recipes
    get 'foods', to: 'foods#index', as: :foods
+   get 'foods/new', to: 'foods#new', as: :new_food
+   post 'foods/create', to: 'foods#create'
+   delete 'foods/:id', to: 'foods#destroy', as: :delete_food
 end
