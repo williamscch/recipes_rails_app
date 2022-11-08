57dbd975cd8a63a747c2bdaa12c9d8f6359b02a0
nimplay <nimrod7day@gmail.com>
2022-11-08 00:54:46 -0400

    fixing linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   protected
 
   def configure_permitted_parameters
