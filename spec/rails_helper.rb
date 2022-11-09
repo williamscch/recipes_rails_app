35c7508332465ff1951cd520f29474be66454515
wdavidcch <david.13.2121@gmail.com>
2022-11-09 19:01:32 -0400

    Set up capybara and add testing for views foods and recipes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,12 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'spec_helper'
+require_relative './spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
-
+require 'capybara/rspec'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -1,16 +1,23 @@
-require 'rails_helper'
+require_relative '../rails_helper'
 
 RSpec.describe 'Recipes', type: :request do
+  before(:example) do
+    User.create(
+      name: 'Jhon First',
+      email: 'jhon@email.com',
+      password: '123456'
+    )
+  end
   describe 'GET /index' do
     it 'returns http success' do
-      get '/recipes/index'
+      get recipes_path
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
     it 'returns http success' do
-      get '/recipes/show'
+      get recipe_path
       expect(response).to have_http_status(:success)
     end
   end
