f1f087628653eeace81440328436b79913a427c9
nimplay <nimrod7day@gmail.com>
2022-11-11 10:38:56 -0400

    fixing requests test

--- spec/requests/logup.rb
+++ spec/requests/logup.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'ShoppingLists', type: :request do
+  subject do
+    User.new(name: 'Tommy', email: 'tommy@email.com', password: '123456')
+  end
+
+  before(:example) { subject.save }
+
   describe 'GET /index' do
     it 'returns http success' do
-      get '/shop'
+      get new_user_registration_path
       expect(response).to have_http_status(:success)
     end
   end
